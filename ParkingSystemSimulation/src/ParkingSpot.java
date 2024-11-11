7e120e58ad203827617789483aac9874c8a02341
Amr-Elharery <amrelharery@gmail.com>
2024-11-11 12:42:12 +0200

    Finish ParkingSpot class

---
+++ ParkingSystemSimulation/src/ParkingSpot.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.Semaphore;
+
+public class ParkingSpot {
+    private Semaphore spots;
+
+    public ParkingSpot(){
+        this.spots = new Semaphore(4, true); // Default value in assignment as well
+    }
+    public ParkingSpot(int spots){
+        this.spots = new Semaphore(spots, true);
+    }
+
+    public boolean tryToPark(){
+        return spots.tryAcquire();
+    }
+
+    public void leave(){
+        spots.release();
+    }
+
+    public int availableSpots(){
+        return spots.availablePermits();
+    }
+}

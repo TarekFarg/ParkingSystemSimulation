b405474c2f49524590571057a210606372dcb686
Amr-Elharery <amrelharery@gmail.com>
2024-11-11 15:00:45 +0200

    Finish Car class & General enhance

--- ParkingSystemSimulation/src/Main.java
+++ ParkingSystemSimulation/src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Create a parking lot with 4 parking spots
+        ParkingSpot parkingSpot = new ParkingSpot(4);
+        String currentDirectory = System.getProperty("user.dir");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try (BufferedReader br = new BufferedReader(new FileReader(Paths.get(currentDirectory, "ParkingSystemSimulation/src/cars.txt").toString()))) {
+            String line;
+            long lastArrivalTime = 0;
+
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(", ");
+                String gate = parts[0];
+                int carId = Integer.parseInt(parts[1].split(" ")[1]);
+                int arrivalTime = Integer.parseInt(parts[2].split(" ")[1]);
+                int stayDuration = Integer.parseInt(parts[3].split(" ")[1]);
+
+                // Ensure the delay is non-negative
+                long delay = (arrivalTime - lastArrivalTime) * 1000;
+                if (delay < 0) {
+                    delay = 0;
+                }
+
+                // Simulate the delay based on the arrival time
+                Thread.sleep(delay);
+                lastArrivalTime = arrivalTime;
+
+                // Start the car thread
+                Car car = new Car(carId, gate, stayDuration, parkingSpot);
+                car.start();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
